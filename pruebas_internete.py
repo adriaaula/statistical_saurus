eb69f6bff23544b718baabe3d93520a4ba4e553a
Adria <adriaaulamart@gmail.com>
2016-03-09 18:36:18 +0100

    Changes in the generate distances .py

--- pruebas_internete.py
+++ pruebas_internete.py
@@ -1,22 +1,34 @@
-import sys
-import os
+import re
 import urllib.parse
 import urllib.request
+from urllib.error import  URLError
 
 url = 'http://www.uniprot.org/uniprot/'
 
 values = {
 'format':'gff',
-'query':'lalala'
+'query':'A0A009ESS6'
 }
 
 data = urllib.parse.urlencode(values)
 data = data.encode('utf8')
 req = urllib.request.Request(url, data)
-response = urllib.request.urlopen(req)
+try:
+    response = urllib.request.urlopen(req)
+except URLError as e:
+    if hasattr(e, 'reason'):
+        print('We failed to reach a server.')
+        print('Reason: ', e.reason)
+    elif hasattr(e, 'code'):
+        print('The server couldn\'t fulfill the request.')
+        print('Error code: ', e.code)
+
 the_page = response.read()
 
-print(the_page)
+the_page = str(the_page)
+
+regex = re.compile("Transmembrane")
+
 
 
 
